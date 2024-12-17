da09e534d61e9252cee6e54c700d2e8c10f780d5
Mateusz Ficek <mateuszficek295@gmail.com>
2024-12-17 14:00:35 +0100

    Release version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,17 @@
+## 1.0.0
+### New Features:
+- Added a `showTooltips` property to enable tooltips on navigation items.
+- Introduced the `padding` property to customize padding around the bottom bar.
+- Changed the horizontalPadding to margin.
+- Added new property cornerRadius.
+- The `onIndexChanged` callback has been added to notify external state listeners when the selected index changes.
+
+### Improvements:
+- Enhanced the `_selectedItemBuilder` to display both the icon and text with better layout styling.
+- Refined `_wrapWithTooltip` to conditionally wrap widgets with `Tooltip` based on the `showTooltips` flag.
+
+### Behavior Updates:
+- The `onSelected` callback now triggers state updates and works alongside `onIndexChanged`.
+
 ## 0.0.1
 Initial Open Source release.
--- lib/src/hidable_bottom_bar.dart
+++ lib/src/hidable_bottom_bar.dart
@@ -1,41 +1,105 @@
 import 'package:flutter/material.dart';
 
+/// HideableBottomBar is a customizable and hideable bottom navigation bar widget for Flutter.
+/// It allows for dynamic updates and a sleek, animated UI.
+/// The widget supports custom child items and styling options, including color, size, and animations.
 class HideableBottomBar extends StatefulWidget {
+  /// Determines the duration of the animation when the bottom bar is shown or hidden.
+  /// Default: Duration(milliseconds: 300)
   final Duration duration;
+
+  /// Allows you to provide a custom widget to use as the bottom bar. If null, a default bottom bar will be used.
+  /// Default: null
   final Widget? bottomBarWidget;
+
+  /// Specifies the currently selected index of the bottom navigation items.
+  /// Required parameter.
   final int selectedIndex;
+
+  /// Sets the background color of the bottom bar.
+  /// Default: Colors.white
   final Color backgroundColor;
+
+  /// Sets the color for unselected navigation items.
+  /// Default: Colors.white
   final Color unselectedColor;
+
+  /// Sets the background color for the selected navigation item.
+  /// Default: Colors.black
   final Color selectedColor;
-  final BoxShadow shadow;
+
+  /// Provides a shadow effect for the bottom bar container.
+  /// Default: [BoxShadow(color: Colors.grey, blurRadius: 16)]
+  final List<BoxShadow> shadow;
+
+  /// Sets the height of the bottom bar.
+  /// Default: 60.0
   final double height;
+
+  /// Configures the text style for the selected item’s label.
+  /// TextStyle(fontSize: 16, color: Colors.white)
   final TextStyle selectedItemTextStyle;
-  final double horizontalPadding;
+
+  /// Sets the horizontal margin for the bottom bar.
+  /// Default: 16.0
+  final double margin;
+
+  /// Controls the vertical position of the bottom bar relative to the bottom of the screen.
+  /// Required parameter.
   final double bottomPosition;
+
+  /// Callback function triggered when a navigation item is selected. It returns the selected HideableBottomNavigationItem.
+  /// Required parameter.
   final Function(HideableBottomNavigationItem) onSelected;
+
+  /// A callback that is triggered whenever the selected index changes.
+  /// This allows external state management for the navigation bar.
+  /// Default: null
+  final Function(int)? onIndexChanged;
+
+  /// List of navigation items displayed in the bottom bar.
+  /// Required parameter.
   final List<HideableBottomNavigationItem> children;
+
+  /// Specifies the animation curve for the bottom bar’s appearance and disappearance.
+  /// Default: Curves.linear
   final Curve? curve;
 
+  /// Specifies the corner radius for the bottom bar and the selected item.
+  /// This allows customization of the widget’s rounded edges.
+  /// Default: 16.0
+  final double cornerRadius;
+
+  /// Determines whether tooltips should be displayed when hovering or long-pressing on navigation items.
+  /// Default: false
+  final bool showTooltips;
+
+  /// Defines the padding around the bottom bar.
+  /// Default: EdgeInsets.symmetric(horizontal: 16.0)
+  final EdgeInsetsGeometry? padding;
+
+  /// Default constructor.
   const HideableBottomBar({
     Key? key,
-    required this.selectedIndex,
+    required this.bottomPosition,
     required this.children,
     required this.onSelected,
-    this.curve,
-    this.height = 60,
+    required this.selectedIndex,
     this.backgroundColor = Colors.white,
-    required this.bottomPosition,
-    this.horizontalPadding = 16,
+    this.bottomBarWidget,
+    this.cornerRadius = 16.0,
+    this.curve,
+    this.duration = const Duration(milliseconds: 300),
+    this.height = 60,
+    this.margin = 16,
+    this.onIndexChanged,
+    this.padding,
     this.selectedColor = Colors.black,
-    this.unselectedColor = Colors.white,
-    this.shadow = const BoxShadow(
-      color: Colors.grey,
-      blurRadius: 16,
-    ),
     this.selectedItemTextStyle =
         const TextStyle(fontSize: 16, color: Colors.white),
-    this.duration = const Duration(milliseconds: 300),
-    this.bottomBarWidget,
+    this.shadow = const [BoxShadow(color: Colors.grey, blurRadius: 16)],
+    this.showTooltips = false,
+    this.unselectedColor = Colors.white,
   }) : super(key: key);
 
   @override
@@ -52,20 +116,21 @@ class _HideableBottomBarState extends State<HideableBottomBar>
     return AnimatedPositioned(
       duration: widget.duration,
       bottom: widget.bottomPosition,
+      left: 0,
+      right: 0,
       curve: widget.curve ?? Curves.linear,
-      left: widget.horizontalPadding,
-      right: widget.horizontalPadding,
       child: widget.bottomBarWidget ?? _defaultBottomBar(),
     );
   }
 
   Widget _defaultBottomBar() {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: widget.padding ?? const EdgeInsets.symmetric(horizontal: 16),
+        margin: EdgeInsets.symmetric(horizontal: widget.margin),
         decoration: BoxDecoration(
           color: widget.backgroundColor,
-          boxShadow: [widget.shadow],
-          borderRadius: BorderRadius.circular(16),
+          boxShadow: widget.shadow,
+          borderRadius: BorderRadius.circular(widget.cornerRadius),
         ),
         height: widget.height,
         child: _bottomBarRow());
@@ -76,70 +141,91 @@ class _HideableBottomBarState extends State<HideableBottomBar>
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: List.generate(
         children.length,
-        (index) {
-          if (index == selectedIndex) {
-            return _selectedItemBuilder(index);
-          }
-          return _unselectedItemBuilder(index);
-        },
+        (index) => index == selectedIndex
+            ? _selectedItemBuilder(index)
+            : _unselectedItemBuilder(index),
       ),
     );
   }
 
-  Center _selectedItemBuilder(int index) {
-    return Center(
-      child: Container(
-        height: widget.height * 2 / 3,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-            color: widget.selectedColor,
-            borderRadius: BorderRadius.circular(16)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _icon(index, widget.unselectedColor),
-            const SizedBox(width: 8),
-            Text(
-              children[index].name,
-              style: widget.selectedItemTextStyle,
-            ),
-          ],
-        ),
-      ),
-    );
+  Widget _wrapWithTooltip({required Widget child, required String message}) {
+    if (widget.showTooltips) {
+      return Tooltip(message: message, child: child);
+    }
+    return child;
   }
 
-  GestureDetector _unselectedItemBuilder(int index) {
-    return GestureDetector(
-      onTap: () {
-        widget.onSelected(children[index]);
-        setState(() {
-          selectedIndex = index;
-        });
-      },
+  Widget _selectedItemBuilder(int index) {
+    return _wrapWithTooltip(
+      message: children[index].name,
       child: Center(
         child: Container(
-          width: 40,
-          height: 40,
+          height: widget.height * 2 / 3,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
           decoration: BoxDecoration(
-            color: widget.unselectedColor,
-            shape: BoxShape.circle,
+              color: widget.selectedColor,
+              borderRadius: BorderRadius.circular(16)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _icon(index, widget.unselectedColor),
+              const SizedBox(width: 8),
+              Text(
+                children[index].name,
+                style: widget.selectedItemTextStyle,
+              ),
+            ],
           ),
-          child: _icon(index, widget.selectedColor),
         ),
       ),
     );
   }
 
-  Icon _icon(int index, Color color) =>
-      Icon(children[index].icon, color: color);
+  Widget _unselectedItemBuilder(int index) {
+    return _wrapWithTooltip(
+      message: children[index].name,
+      child: GestureDetector(
+        onTap: () {
+          widget.onSelected(children[index]);
+          widget.onIndexChanged?.call(index); // Notify external listener
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Center(
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: widget.unselectedColor,
+              shape: BoxShape.circle,
+            ),
+            child: _icon(index, widget.selectedColor),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _icon(int index, Color color) => children[index].icon;
 }
 
+/// HideableBottomNavigationItem represents an individual item in the HideableBottomBar widget.
+/// It defines the properties needed to display the item, including its icon, label, and unique index.
 class HideableBottomNavigationItem {
+  /// The label or name of the navigation item. This text is displayed when the item is selected.
+  /// Required parameter.
   final String name;
-  final IconData icon;
+
+  /// The icon that represents the navigation item. It is displayed for both selected and unselected states.
+  /// Required parameter.
+  final Widget icon;
+
+  /// A unique index identifying this navigation item within the HideableBottomBar. It is used to track the selected item.
+  /// Required parameter.
   final int index;
 
+  /// Default constructor.
   HideableBottomNavigationItem(
       {required this.index, required this.icon, required this.name});
 }
